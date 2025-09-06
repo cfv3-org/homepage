7174bbbaba9289da3667ebf6edf147cc762f0861
Viktor Gievoi <gievoi.v@gmail.com>
2025-09-06 22:43:37 +0200

    Adds NixOS virtual environment

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Node development environment";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+
+  outputs = { self, nixpkgs }:
+  let
+    for = system: let pkgs = import nixpkgs { inherit system; }; in {
+      devShells = {
+        default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.nodejs_20
+            pkgs.yarn
+            pkgs.nodePackages.typescript
+          ];
+        };
+      };
+    };
+  in
+  {
+    devShells.x86_64-linux = (for "x86_64-linux").devShells;
+  };
+}
